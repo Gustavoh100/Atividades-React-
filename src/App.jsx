39dba4f3c02270158b775f17689726c6a31d2d44
Gustavo Henrique <gustavosilva010304@gmail.com>
2026-04-09 22:12:29 -0300

    trabalho de react

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import Footer from './componentes/Footer'
+import Header from './componentes/Header'
+
+function App() {
+  
+  return (
+    <>
+<Header/>
+
+
+
+
+
+
+
+
+
+
+<Footer/>
+</>
+  )
+}
+
+export default App
